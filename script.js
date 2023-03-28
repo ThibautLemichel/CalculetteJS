fa941457aa898fa81398152765ad5c955ff6a86f
ThibautLemichel <thibaut.lemichel@student.junia.com>
2023-03-28 08:08:26 +0200

    Porblème undefined

--- script.js
+++ script.js
@@ -1,24 +1,88 @@
-console.log("Salut beau gosse");
-
-var display = document.querySelector('.display');   // Récupération de la div display
-var display2 = document.querySelector('.display2');   // Récupération de la div display
-var buttons = document.querySelectorAll('button');  // Récupération de tous les boutons
-
-// Ajout d'un écouteur d'événements pour chaque bouton
-buttons.forEach(function(button) {
-    button.addEventListener('click', function() {
-        var value = this.textContent;   // Récupération de la valeur du bouton cliqué
-        // Si le bouton "=" est cliqué, évaluer l'expression mathématique
-        if (value === "=") {
-            var result = eval(display.textContent);
-            display2.textContent = result;   // Affichage du résultat
+class Calculator {
+    constructor() {
+        this.display2 = document.querySelector('.display');
+        this.display = document.querySelector('.display2');
+        this.clearBtn = document.querySelector('.clear');
+        this.parenthesisBtns = document.querySelectorAll('.parenthesis');
+        this.operatorBtns = document.querySelectorAll('.operator');
+        this.numberBtns = document.querySelectorAll('.number');
+        this.equalsBtn = document.querySelector('.equals');
+        this.commaBtn = document.querySelector('.comma');
+        this.previousOperation = "";
+      
+        this.clearBtn.addEventListener('click', this.clear.bind(this));
+        this.parenthesisBtns.forEach(btn => btn.addEventListener('click', this.parenthesis.bind(this)));
+        this.operatorBtns.forEach(btn => btn.addEventListener('click', this.operator.bind(this)));
+        this.numberBtns.forEach(btn => btn.addEventListener('click', this.number.bind(this)));
+        this.equalsBtn.addEventListener('click', this.equals.bind(this));
+        this.commaBtn.addEventListener('click', this.comma.bind(this));
+    }
+  
+    clear() {
+        this.display2.textContent = this.previousOperation;
+        this.display.textContent = "";
+    }
+  
+    parenthesis() {
+        let value = this.display.textContent;
+        if (value === "") {
+            this.display.textContent = "(";
+        } 
+        else {
+            let lastChar = value.slice(-1);
+            if (lastChar === "(") {
+                this.display.textContent = value + "(";
+            } 
+            else if (lastChar === ")") {
+                this.display.textContent = value + "*(";
+            } 
+            else {
+                this.display.textContent = value + ")";
+            }
         }
-        else if(value === "C"){
-            display.textContent = "";
-            display2.textContent = "";
+    }
+  
+    operator() {
+        let value = this.display.textContent;
+        if (value === "") return;
+        let lastChar = value.slice(-1);
+        if (lastChar === "+" || lastChar === "-" || lastChar === "*" || lastChar === "/") {
+            this.display.textContent = value.slice(0, -1) + this.textContent;
+        } 
+        else {
+            this.display.textContent = value + this.textContent;
         }
-        else{
-            display.textContent += value;   // Ajout de la valeur au contenu du display
+    }
+  
+    number() {
+        let value = this.display.textContent;
+        if (value === "0") {
+            this.display.textContent = this.textContent;
+        } 
+        else {
+            this.display.textContent = value + this.textContent;
         }
-    });
-});
+    }
+  
+    equals() {
+        let value = this.display.textContent;
+        if (value === "") return;
+        this.previousOperation = value;
+        let result = eval(value);
+        this.display2.textContent = result;
+        this.display.textContent = "";
+    }
+  
+    comma() {
+        let value = this.display.textContent;
+        if (value === "") {
+            this.display.textContent = "0.";
+        } 
+        else if (!value.includes(".")) {
+            this.display.textContent = value + ".";
+        }
+    }
+}
+  
+let calculator = new Calculator();
+  
